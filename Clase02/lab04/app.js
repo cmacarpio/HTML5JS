3cab5ef7a8b918fee761265ad73744690104a9ae
Claudio Mariano Alejandro Carpio <cmacarpio@gmail.com>
2017-04-07 19:52:02 -0300

    Paso 5

--- Clase02/lab04/app.js
+++ Clase02/lab04/app.js
@@ -1,6 +1,11 @@
-angular.module("lab04",[ ]).controller("sumador",
+angular.module("lab04",[]).controller("sumador",
     function($scope){ 
         $scope.result = 1 + 3 ; 
+        $scope.mitadValor = $scope.result / 2;
+        $scope.randomIncrement=function(){
+            $scope.result+=Math.random();
+            $scope.mitadValor = $scope.result / 2;
+        }
     }).controller("restador",
     function($scope){
         $scope.resta = 3-1;
