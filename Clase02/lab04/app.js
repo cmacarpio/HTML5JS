7d9684a2ed766200c026486675520239197050d5
Claudio Mariano Alejandro Carpio <cmacarpio@gmail.com>
2017-04-07 22:08:31 -0300

    Paso 8

--- Clase02/lab04/app.js
+++ Clase02/lab04/app.js
@@ -3,11 +3,16 @@ angular.module("lab04",[]).controller("sumador",
         $scope.result = 1 + 3 ; 
         $scope.mitadValor = $scope.result / 2;
         $scope.par = (Math.floor($scope.result) % 2) == 0;
+        $scope.indice = 1;    
+        $scope.historial = [{valor:$scope.mitadValor, indice: $scope.indice}]; 
 
         $scope.randomIncrement=function(){
             $scope.result+=Math.random();
             $scope.mitadValor = $scope.result / 2;
             $scope.par = (Math.floor($scope.result) % 2) == 0;
+            $scope.indice++;
+            $scope.historial.push({  valor: $scope.mitadValor,  indice: $scope.indice }); 
+            console.log($scope.historial); 
         }
         
 
