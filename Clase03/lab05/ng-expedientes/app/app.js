ac1a8460afa39e5181044ec591bec8763a9481bb
Claudio Mariano Alejandro Carpio <cmacarpio@gmail.com>
2017-04-26 21:38:53 -0300

    iter01

--- Clase03/lab05/ng-expedientes/app/app.js
+++ Clase03/lab05/ng-expedientes/app/app.js
@@ -1,5 +1,20 @@
 angular.module("appExpedientes",[])
     .controller("areaController",function($scope){
         $scope.titulo = "Gestión de Areas";
+        $scope.listaAreas = []; 
+        $scope.area = undefined; 
+        
+        $scope.cancelar = function(){     
+            $scope.area=undefined; 
+        $scope.editar = function(unArea){     
+            $scope.area=unArea; 
+        } 
+        $scope.guardar = function(){  
+            if($scope.area && $scope.area.nombre){       
+                $scope.listaAreas.push($scope.area);  
+            }else{        
+                $scope.mensaje = "Hubo un problema, el área no puede tener nombre nulo";  
+            } 
+        } 
     }
-);
+});
