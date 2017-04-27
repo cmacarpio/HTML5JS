1de1f92cf28b063a29d59db3e63597cbbd43d175
Claudio Mariano Alejandro Carpio <cmacarpio@gmail.com>
2017-04-27 13:52:26 -0300

    inter03

--- Clase03/lab05/ng-expedientes/app/app.js
+++ Clase03/lab05/ng-expedientes/app/app.js
@@ -1,20 +1,42 @@
 angular.module("appExpedientes",[])
     .controller("areaController",function($scope){
         $scope.titulo = "Gestión de Areas";
-        $scope.listaAreas = []; 
+        $scope.listaAreas = [{"nombre":"Un Area"}]; 
         $scope.area = undefined; 
+        $scope.isEditing=false;
         
         $scope.cancelar = function(){     
-            $scope.area=undefined; 
-        $scope.editar = function(unArea){     
+            console.log('Cancelar click!');
+            $scope.area=undefined;
+            $scope.isEditing=false;
+        }
+        $scope.editar = function(unArea){
+            console.log('Editar click!');
+            $scope.isEditing=true;   
             $scope.area=unArea; 
         } 
-        $scope.guardar = function(){  
-            if($scope.area && $scope.area.nombre){       
+        $scope.guardar = function(){
+            console.log('Guardar click!');
+            if($scope.area && $scope.area.nombre && !$scope.isEditing){       
                 $scope.listaAreas.push($scope.area);  
+            }else if($scope.area && $scope.area.nombre && $scope.isEditing){
+                //Edition
+                alert('Hay que guardar los cambios');
             }else{        
                 $scope.mensaje = "Hubo un problema, el área no puede tener nombre nulo";  
-            } 
+            }
+            $scope.area={};
+            $scope.isEditing=false;
         } 
-    }
+
+        $scope.nueva = function(){
+            console.log('Nueva click!');
+            $scope.area={};
+            $scope.isEditing=false;
+        }
+        $scope.borrar = function(unArea){
+            console.log('Borrar click!');
+            $scope.isEditing=false;
+        }
+    
 });
