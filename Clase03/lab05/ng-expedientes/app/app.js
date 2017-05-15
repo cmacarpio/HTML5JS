50c33f47a7bfe8ac57ef58ad1d7dab984577bfac
Claudio Mariano Alejandro Carpio <cmacarpio@gmail.com>
2017-05-15 12:18:03 -0300

    Cambios 15/05/2017

--- Clase03/lab05/ng-expedientes/app/app.js
+++ Clase03/lab05/ng-expedientes/app/app.js
@@ -1,50 +1,40 @@
 angular.module("appExpedientes",[])
     .controller("areaController",function($scope){
         $scope.titulo = "Gestión de Areas";
-        $scope.listaAreas = [{"nombre":"Un Area"}]; 
+        $scope.listaAreas = []; //Aárea de prueva {"nombre":"Un Area"}
         $scope.area = undefined; 
         $scope.isEditing=false;        
         
         $scope.cancelar = function(){     
-            console.log('Cancelar click!');
             $scope.area=undefined;
             $scope.isEditing=!$scope.isEditing;
-            console.log("Cancelar "+$scope.isEditing);
         }
         $scope.editar = function(unArea){
-            console.log('Editar click!');
             $scope.isEditing=true;   
             $scope.area=unArea; 
-            console.log("Editar "+$scope.isEditing);
         } 
         $scope.guardar = function(){
-            console.log('Guardar click!'); //&& !$scope.isEditing
-            console.log($scope.area);
-            //if($scope.area === {} && $scope.area.nombre && $scope.isEditing){   
             if( $scope.listaAreas.indexOf($scope.area) < 0){    
                 $scope.listaAreas.push($scope.area);   
             }else if($scope.area!=={} && $scope.area.nombre && $scope.isEditing){
                 //Edition
-                //alert('Hay que guardar los cambios');
             }else{        
                 $scope.mensaje = "Hubo un problema, el área no puede tener nombre nulo";  
             }
             $scope.area={};
             $scope.isEditing=!$scope.isEditing;
-            console.log("Guardar "+$scope.isEditing);
         } 
 
         $scope.nueva = function(){
-            console.log('Nueva click!');
             $scope.area={};
             $scope.isEditing=!$scope.isEditing;
-            console.log("Nueva "+$scope.isEditing);
-            console.log($scope.area);
         }
         $scope.borrar = function(unArea){
-            console.log('Borrar click!');
+            var idx = $scope.listaAreas.indexOf(unArea);
+            if( idx >= 0){
+                $scope.listaAreas.splice(idx,1);
+            }
             $scope.isEditing=false;
-            console.log("Borrar "+$scope.isEditing);
         }
     
 });
