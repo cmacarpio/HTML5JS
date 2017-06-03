6c9463f860607f6b0f6b597fa26b01df281cdfa4
Claudio Mariano Alejandro Carpio <cmacarpio@gmail.com>
2017-06-03 20:19:10 -0300

    mejoras

--- Clase04/lab07/ng-expedientes/app/servicios/areaService.js
+++ Clase04/lab07/ng-expedientes/app/servicios/areaService.js
@@ -2,12 +2,14 @@ angular.module("appExpedientes")
     .service("areaService",function(){
         this.areas=[{"nombre":"Un Area"},{"nombre":"Salud e higiene"}];//[];
         this.addArea = function(unArea){
+            console.log('Se agregará un área :'+unArea.nombre);
             this.areas.push(unArea);
         }
         this.listarAreas = function(){
             return this.areas;
         }
         this.borrarArea = function(idx){
+            console.log('Se borrará el elemento en la posición :'+idx);
             this.areas.splice(idx,1);
         }
     });
