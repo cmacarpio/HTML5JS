cadd6baca3a16d7cbeef3b863e14d2d32cf9a4aa
Claudio Mariano Alejandro Carpio <cmacarpio@gmail.com>
2017-06-19 13:42:34 -0300

    Lab11

--- Clase06/lab11/procesar.js
+++ Clase06/lab11/procesar.js
@@ -4,6 +4,6 @@ module.exports.procesarURL =  function procesarURL(req, res, next) {
 	var query = url_parts.query;
 	req.op1=query.op1;
 	req.op2=query.op2;
-	res.write("<html><head><title>Calculadora NodeJS</title></head><body>");
+	res.write("<html><head><title>Calculadora NodeJS</title></head><body>");	
 	next();//completar para que siga procesando
 }
--- Clase06/lab11/servidor.js
+++ Clase06/lab11/servidor.js
@@ -6,13 +6,13 @@ connect()
  .use("/operacion/sumar",calculadora.sumar)
  .use("/operacion/restar",calculadora.restar)
  .use("/operacion/multiplicar",calculadora.multiplicar)
- .use("/",function (req, res) {
+ .use("/",function (req, res) {	 		
 			console.log(req.resultado);
 		if(req.resultado){
 			res.write("<h1>Resultado. </h1>: "+ req.resultado+"</body></html>");
 			res.end();
 		}else{
-			res.end("<html><head></head><body>Operacion no coincide con las soliciitadas !!! </body></html>");
+			res.end("<html><head></head><body>Operacion no coincide con las solicitadas !!! </body></html>");
 		}
  })
  .listen(8081);
